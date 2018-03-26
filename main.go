aa33c9a4db6675f2801b0b26b4d811d9a8feee4b
Keisuke Kishida <kishidak@tcd.ie>
2018-03-26 22:59:28 +0900

    initial commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/json", json)
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func json(w http.ResponseWriter, r *http.Request) {
+	f, e := os.Open("output.json")
+	if e != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
+}
