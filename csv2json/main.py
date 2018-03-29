2cb70febee17e3987aa41d41e60f2485bedfdb0e
Keisuke Kishida <kishidak@tcd.ie>
2018-03-29 17:31:56 +0900

    Changed JSON format / Add csv2json script

---
+++ csv2json/main.py
@@ -0,0 +1,25 @@
+# coding:UTF-8
+import json
+
+name = "情報セキュリティマネジメント"
+cards = []
+count = 1
+
+# CSVファイルのロード
+with open('./input.csv', 'r') as file:
+    # list of dictの作成
+    for line in file:
+        # Split a line into words
+        words = line.split(',')
+        card = {}
+        card["key"] = count
+        card["front"] = str(words[1])
+        card["back"] = str(words[3])
+
+        cards.append(card)
+        count += 1
+
+with open('../output.json', 'w') as f:
+    # JSONへの書き込み
+    deck = [{"name": name, "cards": cards}]
+    json.dump(deck, f, ensure_ascii=False)
